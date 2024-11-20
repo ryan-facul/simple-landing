cc971d3d61ca21b113421554d557dd1289f577e5
ryan-facul <142547892+ryan-facul@users.noreply.github.com>
2024-11-20 20:19:04 -0300

    Update contato.tsx

--- src/components/contato.tsx
+++ src/components/contato.tsx
@@ -15,7 +15,7 @@ export default function Contato() {
         console.log('Teste 1 = ' + JSON.stringify(data));
         
         try {
-            const response = await fetch('https://testesandro-678569712894.us-central1.run.app', {
+            const response = await fetch('https://testesandro-678569712894.us-central1.run.app/receba', {
             method: 'POST',
             headers: {'Content-Type':'application/json'},
             mode: 'no-cors',
