3248a28271a6ba0f883d07b04f646c157cb874b4
ryan-facul <142547892+ryan-facul@users.noreply.github.com>
2024-11-21 00:15:44 -0300

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,22 +1,26 @@
 import "../styles/footer.css";
+import Face from "../assets/images/317727_facebook_social media_social_icon.svg";
+import Insta from "../assets/images/1298747_instagram_brand_logo_social media_icon.svg";
+import X from "../assets/images/11244080_x_twitter_elon musk_twitter new logo_icon.svg";
+import Link from "../assets/images/5296501_linkedin_network_linkedin logo_icon.svg";
 
 export default function Footer() {
     return (
         <footer>
-            <h3>Nome da Empresa</h3>
+            <h3>Overtech</h3>
             <p>&copy; 2024 Todos os direitos reservados.</p>
 
             <div className="footer-links">
-            <a href="#">Sobre nós</a>
-            <a href="#">Política de Privacidade</a>
-            <a href="#">Termos de Serviço</a>
+                <a href="#">Sobre nós</a>
+                <a href="#">Política de Privacidade</a>
+                <a href="#">Termos de Serviço</a>
             </div>
 
             <div className="footer-socials">
-            <a href="#" aria-label="Facebook">F</a>
-            <a href="#" aria-label="Instagram">I</a>
-            <a href="#" aria-label="Twitter">T</a>
-            <a href="#" aria-label="LinkedIn">L</a>
+                <a href="#" aria-label="Facebook"><img src={Face} alt="facebook" /></a>
+                <a href="#" aria-label="Instagram"><img src={Insta} alt="instagram" /></a>
+                <a href="#" aria-label="X"><img src={X} alt="x" /></a>
+                <a href="#" aria-label="LinkedIn"><img src={Link} alt="linkedin" /></a>
             </div>
         </footer>
     )
