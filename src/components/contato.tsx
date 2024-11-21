2470faf15a12491a1ddacc3a0f15ce076e329043
ryan-facul <142547892+ryan-facul@users.noreply.github.com>
2024-11-20 22:05:53 -0300

    Update contato.tsx

--- src/components/contato.tsx
+++ src/components/contato.tsx
@@ -52,15 +52,16 @@ export default function Contato() {
                     <h2>Produtos exclusivos para você</h2>
                 </span>
                 <p>Forneça seu nome e e-mail abaixo para receber ofertas e produtos exclusivos, saiba mais sobre nossa empresa esteja mais próximo das soluções ideais para você.</p>
+                <span className="input">
+                    <input type="text" value={email} 
+                        onChange={event => setEmail(event.target.value)} placeholder="Seu e-mail" />
+                    <input type="text" value={nome}
+                        onChange={event => setNome(event.target.value)} placeholder="Seu nome"/>
+                    
+                    <Send text="Assinar" func={handleSubmit} />
+                </span>
             </header>
-            <span className="content input">
-                <input type="text" value={email} 
-                    onChange={event => setEmail(event.target.value)} placeholder="Seu e-mail" />
-                <input type="text" value={nome}
-                    onChange={event => setNome(event.target.value)} placeholder="Seu nome"/>
-                
-                <Send text="Assinar" func={handleSubmit} />
-            </span>
+            
         </>
     )
 }
