589b4a4ab7ed1874820975fa596f8b0b68779d17
ryan-facul <142547892+ryan-facul@users.noreply.github.com>
2024-11-20 21:04:37 -0300

    Update contato.tsx

--- src/components/contato.tsx
+++ src/components/contato.tsx
@@ -22,7 +22,6 @@ export default function Contato() {
             method: 'POST',
             headers: {
                 'Content-Type':'application/json',
-                'Access-Control-Allow-Origin':'*',
             },
             body: JSON.stringify(data),
             });
